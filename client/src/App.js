7cbfc3b2c784ce0342d6ca079c1c6b847e226992
Johnydavid <dropin2johny@gmail.com>
2023-05-17 20:22:48 +0530

    c

--- client/src/App.js
+++ client/src/App.js
@@ -43,9 +43,9 @@ const App = () => {
     axios.get(url)
       .then((res) => {
         console.log(res);
-        setUser(res.data[0]
+        setUser(res.user
           );
-        console.log(res.data[0]
+        console.log(res.user
           
           )
         
